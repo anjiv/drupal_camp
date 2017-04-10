6bd6c8cadde1150bebafa5472a302eb670989827
Anjali vijay <anjali.vijay@qed42.com>
2017-04-10 10:41:47 +0530

    hamburger menu added

--- themes/internship2017/config.rb
+++ themes/internship2017/config.rb
@@ -10,9 +10,10 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
+sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
---
+++ themes/internship2017/js/scripts.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+  jQuery('.hamburger-menu').on('click', function(){
+    jQuery('#block-internship2017-main-menu .menu').toggleClass('active');
+  });
+  jQuery('.hamburger-close').on('click', function(){
+    jQuery('#block-internship2017-main-menu .menu').toggleClass('active');
+  });
+
+});
